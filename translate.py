a34d25ee346212ba7ab046dad5167fa908051a32
jordanxlau <jordanxlau@yahoo.ca>
2024-12-26 19:28:59 -0500

    bugfix: edited filepath of DeepL auth key

--- translate.py
+++ translate.py
@@ -4,7 +4,7 @@ import deepl
 def deepl_translate(text):
     if text == "":
         return ""
-    auth_key = open("/Users/jorda/Documents/deepl_auth_key.txt").read()
+    auth_key = open("deepl_auth_key.txt").read()
     translator = deepl.Translator(auth_key) 
     translation = translator.translate_text(text, target_lang="fr") 
     return translation.text
